14c65188860b7d4dbce805d369e2f1133bfcb8d0
Nicolas Eidi <nicolas27@outlook.com>
2024-07-18 01:35:06 -0300

    Lista Encadeada Simples

--- Lista-Encadeada-Simples.c
+++ Lista-Encadeada-Simples.c
@@ -1,16 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct No{
+typedef struct no{
     int valor;
-    struct No proximo;
+    struct no *proximo;
 }No;
 
+No *cria_no(int valor);
+void imprime(No *inicio);
 
 int main()
 {
+    No *inicio = NULL;
+    inicio = cria_no(10);
 
-
+    imprime(inicio);
 
     return 0;
+}
+
+No *cria_no(int valor)
+{
+    No *novo = NULL; 
+    novo = malloc(sizeof(No));
+    if (novo == NULL)
+        return NULL;
+
+    novo->valor = valor;
+    novo->proximo = NULL;
+    return novo;
+
+    
+}
+
+void imprime(No *inicio)
+{
+
+    while(inicio != NULL)
+    {
+        printf("valor: %d\n", inicio->valor);
+        inicio = inicio->proximo;
+    }
 }
